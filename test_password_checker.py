7d427cda317f6c42ee085f08e9e3900df5f67a34
Donk <nicetameyerl@gmail.com>
2026-09-11 11:28:05 +0200

    Update test_password_checker.py
    add Blackbox Test

--- test_password_checker.py
+++ test_password_checker.py
@@ -1,1 +1,8 @@
+from password_checker import is_valid_password
+
+assert is_valid_password("password1") == False
+assert is_valid_password("Password") == False
+assert is_valid_password("PassW1") == False
+assert is_valid_password("PASSWORD1") == False
+assert is_valid_password("Password1") == True
 
